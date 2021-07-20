a2caeaf00a17c2231a49354c8a6dd8af189fc7cd
Riley <adams.riley@rogers.com>
2021-07-19 20:14:49 -0400

    Created Source File

---
+++ 190416070_190824620_a04.c
@@ -0,0 +1,12 @@
+/*
+-------------------------------------
+File:   Question1.c
+Project: CP386 Assignment 2
+Creates threads in a function and with the main and prints which is active, each runs 5 times
+-------------------------------------
+Author:  Riley Adams & Torin Borton-McCallum
+ID:      190416070 & 190824620
+Email:   adam6070@mylaurier.ca & bort4620@mylaurier.ca
+Version  2021-06-18
+-------------------------------------
+ */
